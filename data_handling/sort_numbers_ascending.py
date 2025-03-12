222cbc22bdfb577370450f7c30e6859804684de8
John Mike Asuncion <johnmikeasuncion17@gmail.com>
2025-03-12 20:49:27 +0800

    initial commit

---
+++ data_handling/sort_numbers_ascending.py
@@ -0,0 +1,7 @@
+# Prog05: Create a program that ask user to input a number, 
+# continue asking until the user input is invalid. 
+# Display the number from lowest to highest. Clue: sort() function
+
+# ------------------------------------------------------------------
+
+# Phase 1: Planning
