d4cedff0907b705e4a2ccc95fd51bff4f09eae37
John Mike Asuncion <johnmikeasuncion17@gmail.com>
2025-03-12 21:34:52 +0800

    docs: renamed batch 3 file folder

---
+++ data_handling_techniques/display_unique_numbers.py
@@ -0,0 +1,6 @@
+# Prog01: Create a program that ask user to input 10 numbers. 
+# Display all numbers that don't have duplicate.
+
+# ------------------------------------------------------------------
+
+# Phase 1: Planning
